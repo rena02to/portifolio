c8cd61d44bb9be558419714ee65dbb051eb7f956
Renato Alves <rena02to@gmail.com>
2024-03-25 09:40:22 -0300

    Removendo o icone inserido no último commit

--- src/components/start.tsx
+++ src/components/start.tsx
@@ -1,28 +1,9 @@
-import Image from 'next/image';
 import style from './../styles/start.module.scss';
-import { useEffect } from 'react';
 
 export default function Start(){
-    //deixar o texto circular
-    useEffect(() => {
-        const text = document.getElementById('name');
-        if(text){
-            text.innerHTML = text.innerText.split("").map(
-                (char, i) =>
-                `<span style="transform:rotate(${i * 6.5}deg)">${char === " " ? "&nbsp;" : char}</span>`
-            ).join("");
-        }
-    }, []);
-
     return(
         <section className={style.home}>
-            <div className={style.containerIcon}>
-                <div className={style.name}>
-                    <p id='name'>Renato Alves - Portifólio - </p>
-                </div>
-                <Image alt='Hacker icon' src={'/images/hacker.png'} height={45} width={45} />
-            </div>
-            <div className={style.open}>
+             <div className={style.open}>
                 <span/>
                 <p>Disponível para trabalhos</p>
             </div>
