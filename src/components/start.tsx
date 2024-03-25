101d41a1b4cb67384a0c2069716fc7dc2bd06cfd
Renato Alves <rena02to@gmail.com>
2024-03-25 09:04:25 -0300

    inserindo o ícone de disponível para trabalhos

---
+++ src/components/start.tsx
@@ -0,0 +1,13 @@
+import style from './../styles/start.module.scss';
+
+export default function Start(){
+
+    return(
+        <section className={style.home}>
+            <div className={style.open}>
+                <span/>
+                <p>Disponível para trabalhos</p>
+            </div>
+        </section>
+    );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
 import style from "@/styles/Home.module.scss";
 import Navbar from "@/components/navbar";
+import Start from "@/components/start";
 
 export default function Home() {
   return (
     <main className={style.main}>
       <Navbar />
+      <Start />
     </main>
   );
 }
