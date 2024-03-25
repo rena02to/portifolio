0361da338bdb5f6f79892eda221c6c89d4f5f0d1
Renato Alves <rena02to@gmail.com>
2024-03-25 09:45:13 -0300

    corrigindo um erro de responsividade

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,7 +10,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return(
     <>
       <Head>
-        <title>Renato Alves</title>
+        <title>Renato Alves | Portifólio</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
       <Analytics />
