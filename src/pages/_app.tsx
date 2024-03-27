80a3d821854d0dd7ada90a05cc4afce63d3dc907
Renato Alves <rena02to@gmail.com>
2024-03-27 14:05:26 -0300

    Inserindo o SpeedInsights

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import "@/styles/globals.scss";
 import type { AppProps } from "next/app";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import Head from "next/head";
 import { store, persistor } from './../redux/store';
 import { Provider } from "react-redux";
@@ -14,6 +15,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
       <Analytics />
+      <SpeedInsights />
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
           <Component {...pageProps} />
