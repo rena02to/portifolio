15bf001d69cef7144556c23707c9040faf15b5e0
Renato Alves <rena02to@gmail.com>
2024-03-28 13:06:23 -0300

    removendo o que foi inserido com a finalidade de testar o botão de voltar ap topo.

--- src/components/start.tsx
+++ src/components/start.tsx
@@ -2,7 +2,7 @@ import style from './../styles/start.module.scss';
 
 export default function Start(){
     return(
-        <section className={style.home} id='projetos'>
+        <section className={style.home}>
             <div className={style.open}>
                 <span/>
                 <p>Disponível para trabalhos</p>
